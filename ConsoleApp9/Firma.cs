d9316a43253733c89909c1792558187be9905b1d
SpectrA <iliybalabol@gmail.com>
2022-03-10 16:38:18 +0300

    добавлено содержимое класса Firma

--- ConsoleApp9/Firma.cs
+++ ConsoleApp9/Firma.cs
@@ -8,7 +8,20 @@ namespace Practica
 {
     class Firma
     {
+        public string FName { set; get; }
 
+        public string FAdres { set; get; }
+
+        public int FGod { set; get; }
+
+        public Firma() { }
+
+        public Firma(string FName, string FAdres, int FGod)
+        {
+            FName = this.FName;
+            FAdres = this.FAdres;
+            FGod = this.FGod;
+        }
     }
 }
 
