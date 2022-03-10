75ad4a0c165beca0812348edbb864207e0faa44f
SpectrA <iliybalabol@gmail.com>
2022-03-10 15:14:20 +0300

    Initial commit

---
+++ ConsoleApp9/Firma.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica
+{
+    class Firma
+    {
+        public string FName { set; get; }
+
+        public string FAdres { set; get; }
+
+        public int FGod { set; get; }
+
+        public Firma() { }
+
+        public Firma(string FName, string FAdres, int FGod)
+        {
+            FName = this.FName;
+            FAdres = this.FAdres;
+            FGod = this.FGod;
+        }
+
+    }
+}
+
---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Firma Firma = new Firma();
+            Firma.FName = "Nike";
+            Firma.FAdres = "США: Бивертон(Орегон)";
+            Firma.FGod = 1964;
+            Console.WriteLine($"{Firma.FName}  {Firma.FAdres}  {Firma.FGod}");
+            Firma fir1 = new Firma("Adidas", "GeBruder Dassler Schuhfabrik", 2020);
+            Console.WriteLine($"{Firma.FName}  {Firma.FAdres}  {Firma.FGod}");
+            Console.ReadKey();
+        }
+    }
+}
