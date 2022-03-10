36d9fb672b87ca8dc3508248ceaa2e1369c75108
SpectrA <iliybalabol@gmail.com>
2022-03-10 16:39:47 +0300

    добавлен метод Main

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -10,7 +10,14 @@ namespace Practica
     {
         static void Main(string[] args)
         {
-
+            Firma Firma = new Firma();
+            Firma.FName = "Nike";
+            Firma.FAdres = "США: Бивертон(Орегон)";
+            Firma.FGod = 1964;
+            Console.WriteLine($"{Firma.FName}  {Firma.FAdres}  {Firma.FGod}");
+            Firma fir1 = new Firma("Adidas", "GeBruder Dassler Schuhfabrik", 2020);
+            Console.WriteLine($"{Firma.FName}  {Firma.FAdres}  {Firma.FGod}");
+            Console.ReadKey();
         }
     }
 }
