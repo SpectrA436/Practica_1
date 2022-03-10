bd184471e7759e47859217d4f4646ddf65dbd766
SpectrA <iliybalabol@gmail.com>
2022-03-10 16:31:57 +0300

    'init'

--- ConsoleApp9/Firma.cs
+++ ConsoleApp9/Firma.cs
@@ -8,20 +8,6 @@ namespace Practica
 {
     class Firma
     {
-        public string FName { set; get; }
-
-        public string FAdres { set; get; }
-
-        public int FGod { set; get; }
-
-        public Firma() { }
-
-        public Firma(string FName, string FAdres, int FGod)
-        {
-            FName = this.FName;
-            FAdres = this.FAdres;
-            FGod = this.FGod;
-        }
 
     }
 }
